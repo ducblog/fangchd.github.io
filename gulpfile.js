d8e474c43860cd469112fad3f2fd121e4c6f4749
fangchd <idfc@qq.com>
2016-11-04 21:17:29 +0800

    1. put google analytics js file to local site 2. add gulp to combine and minify js/css 3. put js to the end, put css to the beginning.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
+const cleanCss = require('gulp-clean-css');
+
+const jsFiles = [
+    'assets/vendor/jquery/dist/jquery.min.js',
+    'assets/js/jquery-ui.js',
+    'assets/js/lunr.min.js',
+    'assets/js/search.js',
+    'assets/vendor/share.js/dist/js/share.min.js',
+    'assets/js/geopattern.js',
+    'assets/js/prism.js',
+];
+
+gulp.task('pack-js', function () {
+    return gulp.src(jsFiles)
+		.pipe(concat('bundle.js'))
+		.pipe(minify({ ext: { min: '.js' }, noSource: true }))
+		.pipe(gulp.dest('assets/static'));
+});
+
+gulp.task('pack-css', function () {
+    return gulp.src('assets/css/index.css')
+		.pipe(cleanCss({compatibility: 'ie8', keepSpecialComments: 0}))
+		.pipe(concat('bundle.css'))
+        .pipe(gulp.dest('assets/static'));
+});
+
+gulp.task('default', ['pack-js', 'pack-css']);
